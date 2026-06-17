ffc2a03c630aeb2b5226d9199de62682118f901e
supalibit <muhammadalli.ma694@gmail.com>
2026-06-17 19:30:11 +0700

    Update Histats ID in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,13 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Histats.com START (aync) - ID: 5019431 */}
+        {/* Histats.com START (async) - ID Baru: 5033731 */}
         <script 
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
               var _Hasync= _Hasync|| [];
-              _Hasync.push(['Histats.start', '1,5019431,4,0,0,0,00010000']);
+              _Hasync.push(['Histats.start', '1,5033731,4,0,0,0,00010000']);
               _Hasync.push(['Histats.fasi', '1']);
               _Hasync.push(['Histats.track_hits', '']);
               (function() {
@@ -26,10 +26,10 @@ export default function MyApp({ Component, pageProps }) {
 
       <Component {...pageProps} />
 
-      {/* Noscript Fallback */}
+      {/* Noscript Fallback ID Baru */}
       <noscript>
         <a href="/" target="_blank">
-          <img src="//sstatic1.histats.com/0.gif?5019431&101" alt="histats" border="0" />
+          <img src="//sstatic1.histats.com/0.gif?5033731&101" alt="histats" border="0" />
         </a>
       </noscript>
     </>
